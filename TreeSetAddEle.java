d329666108bbd5d5571384aad1ddcd2a231e8c08
vaishnavi kane <109944617+vaishu009@users.noreply.github.com>
2022-08-04 22:32:06 +0530

    tree set, add some colors & print out tree set.

---
+++ TreeSetAddEle.java
@@ -0,0 +1,25 @@
+
+import java.util.TreeSet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class TreeSetAddEle {
+   public static void main(String[] args) {
+  TreeSet<String> treeset = new TreeSet<String>();
+  treeset.add("Red");
+  treeset.add("Green");
+  treeset.add("Orange");
+  treeset.add("White");
+  treeset.add("Black");
+  System.out.println("Tree set: ");
+  System.out.println(treeset);
+ } 
+}
