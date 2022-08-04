f115f634b858987c0f422cf6806d160002f10f20
vaishnavi kane <109944617+vaishu009@users.noreply.github.com>
2022-08-04 22:15:46 +0530

    get the number of elements in a hash set.

---
+++ HashSet.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class HashSet {
+    public static void main(String[] args) {
+         ArrayList<String> set = new ArrayList<String>();
+          set.add("Red");
+          set.add("Green");
+          set.add("Black");
+          set.add("White");
+          set.add("Pink");
+          set.add("Yellow");
+    System.out.println("Original Hash Set: " + set);
+    System.out.println("Size of the Hash Set: " + set.size());
+   }
+}
